7d080b6f00ac20516f4b94146d86fd370a4aefa7
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-10-06 12:02:18 +0200

    Create loop functions

--- js/drive-alone.js
+++ js/drive-alone.js
@@ -1,1 +1,19 @@
-const load = function() {}
+const load = function() {
+    const canvas = document.getElementById('world')
+    canvas.width = 300
+    canvas.height = 300
+    const world = canvas.getContext('2d')
+    const data = {}
+
+    setInterval(function() {
+        update(data)
+        draw(data, world, canvas.width, canvas.height)
+    }, 1000)
+}
+
+const update = function(data) {}
+
+const draw = function(data, world, width, height) {
+    world.fillStyle = '#eee'
+    world.fillRect(0, 0, width, height)
+}
