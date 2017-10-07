3d00aab85be046886b6e0983d54d4148ee5bec68
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-10-07 19:53:32 +0200

    Switch from functions to class

--- test/TestPoint.js
+++ test/TestPoint.js
@@ -2,127 +2,127 @@ class TestPoint {
 
     testCreateOrigin() {
         const origin = Point(0, 0)
-        assertDefined(origin.x)
-        assertDefined(origin.y)
-        assertEqual(0, origin.x)
-        assertEqual(0, origin.y)
+        UnitTest.assertDefined(origin.x)
+        UnitTest.assertDefined(origin.y)
+        UnitTest.assertEqual(0, origin.x)
+        UnitTest.assertEqual(0, origin.y)
     }
 
     testCreate() {
         const point = Point(4, 5)
-        assertDefined(point.x)
-        assertDefined(point.y)
-        assertEqual(4, point.x)
-        assertEqual(5, point.y)
-        assertEqual(Point(4, 5), point)
+        UnitTest.assertDefined(point.x)
+        UnitTest.assertDefined(point.y)
+        UnitTest.assertEqual(4, point.x)
+        UnitTest.assertEqual(5, point.y)
+        UnitTest.assertEqual(Point(4, 5), point)
     }
 
     testNegateOrigin() {
         const point = Point(0, 0)
-        assertEqual(Point(0, 0), point.negate())
+        UnitTest.assertEqual(Point(0, 0), point.negate())
     }
 
     testNegate() {
         const point = Point(4, 5)
-        assertEqual(Point(-4, -5), point.negate())
+        UnitTest.assertEqual(Point(-4, -5), point.negate())
     }
 
     testNegateNotModify() {
         const point = Point(4, 5)
         point.negate()
-        assertEqual(Point(4, 5), point)
+        UnitTest.assertEqual(Point(4, 5), point)
     }
 
     testTranslateOrigin() {
         const point = Point(4, 5)
-        assertEqual(Point(4, 5), point.translate())
+        UnitTest.assertEqual(Point(4, 5), point.translate())
     }
 
     testTranslate() {
         const point = Point(4, 5)
-        assertEqual(Point(7, -1), point.translate(Point(3, -6)))
+        UnitTest.assertEqual(Point(7, -1), point.translate(Point(3, -6)))
     }
 
     testTranslateNotModify() {
         const point = Point(4, 5)
         point.translate(Point(1, 1))
-        assertEqual(Point(4, 5), point)
+        UnitTest.assertEqual(Point(4, 5), point)
     }
 
     testRotateNoArgument() {
         const point = Point(1, 0)
-        assertEqual(Point(Math.sqrt(3) / 2, 1 / 2), point.rotate(Math.PI / 6), 1e-6)
-        assertEqual(Point(Math.sqrt(2) / 2, Math.sqrt(2) / 2), point.rotate(Math.PI / 4), 1e-6)
-        assertEqual(Point(1 / 2, Math.sqrt(3) / 2), point.rotate(Math.PI / 3), 1e-6)
-        assertEqual(Point(0, 1), point.rotate(Math.PI / 2), 1e-6)
-        assertEqual(Point(-1, 0), point.rotate(Math.PI), 1e-6)
-        assertEqual(Point(1, 0), point.rotate(2 * Math.PI), 1e-6)
+        UnitTest.assertEqual(Point(Math.sqrt(3) / 2, 1 / 2), point.rotate(Math.PI / 6), 1e-6)
+        UnitTest.assertEqual(Point(Math.sqrt(2) / 2, Math.sqrt(2) / 2), point.rotate(Math.PI / 4), 1e-6)
+        UnitTest.assertEqual(Point(1 / 2, Math.sqrt(3) / 2), point.rotate(Math.PI / 3), 1e-6)
+        UnitTest.assertEqual(Point(0, 1), point.rotate(Math.PI / 2), 1e-6)
+        UnitTest.assertEqual(Point(-1, 0), point.rotate(Math.PI), 1e-6)
+        UnitTest.assertEqual(Point(1, 0), point.rotate(2 * Math.PI), 1e-6)
     }
 
     testRotate() {
         const point = Point(4, 5)
         const center = Point(3, 5)
-        assertEqual(Point(3 + Math.sqrt(3) / 2, 5 + 1 / 2), point.rotate(Math.PI / 6, center), 1e-6)
-        assertEqual(Point(3 + Math.sqrt(2) / 2, 5 + Math.sqrt(2) / 2), point.rotate(Math.PI / 4, center), 1e-6)
-        assertEqual(Point(3 + 1 / 2, 5 + Math.sqrt(3) / 2), point.rotate(Math.PI / 3, center), 1e-6)
-        assertEqual(Point(3, 6), point.rotate(Math.PI / 2, center), 1e-6)
-        assertEqual(Point(2, 5), point.rotate(Math.PI, center), 1e-6)
-        assertEqual(Point(4, 5), point.rotate(2 * Math.PI, center), 1e-6)
+        UnitTest.assertEqual(Point(3 + Math.sqrt(3) / 2, 5 + 1 / 2), point.rotate(Math.PI / 6, center), 1e-6)
+        UnitTest.assertEqual(Point(3 + Math.sqrt(2) / 2, 5 + Math.sqrt(2) / 2), point.rotate(Math.PI / 4, center), 1e-6)
+        UnitTest.assertEqual(Point(3 + 1 / 2, 5 + Math.sqrt(3) / 2), point.rotate(Math.PI / 3, center), 1e-6)
+        UnitTest.assertEqual(Point(3, 6), point.rotate(Math.PI / 2, center), 1e-6)
+        UnitTest.assertEqual(Point(2, 5), point.rotate(Math.PI, center), 1e-6)
+        UnitTest.assertEqual(Point(4, 5), point.rotate(2 * Math.PI, center), 1e-6)
 
     }
 
     testRotateNotModify() {
         const point = Point(4, 5)
         point.rotate(1)
-        assertEqual(Point(4, 5), point)
+        UnitTest.assertEqual(Point(4, 5), point)
     }
 
     testMeasureAngleNoArgument() {
         const point1 = Point(1, 0)
-        assertEqual(0, point1.measureAngle(), 1e-6)
+        UnitTest.assertEqual(0, point1.measureAngle(), 1e-6)
 
         const point2 = Point(Math.sqrt(3), 1)
-        assertEqual(Math.PI / 6, point2.measureAngle(), 1e-6)
+        UnitTest.assertEqual(Math.PI / 6, point2.measureAngle(), 1e-6)
 
         const point3 = Point(1, 1)
-        assertEqual(Math.PI / 4, point3.measureAngle(), 1e-6)
+        UnitTest.assertEqual(Math.PI / 4, point3.measureAngle(), 1e-6)
 
         const point4 = Point(1, Math.sqrt(3))
-        assertEqual(Math.PI / 3, point4.measureAngle(), 1e-6)
+        UnitTest.assertEqual(Math.PI / 3, point4.measureAngle(), 1e-6)
 
         const point5 = Point(0, 1)
-        assertEqual(Math.PI / 2, point5.measureAngle(), 1e-6)
+        UnitTest.assertEqual(Math.PI / 2, point5.measureAngle(), 1e-6)
 
         const point6 = Point(-1, 0)
-        assertEqual(Math.PI, point6.measureAngle(), 1e-6)
+        UnitTest.assertEqual(Math.PI, point6.measureAngle(), 1e-6)
     }
 
     testMeasureAngle() {
         const center = Point(4, 5)
         const point1 = Point(5, 5)
-        assertEqual(0, point1.measureAngle(center), 1e-6)
+        UnitTest.assertEqual(0, point1.measureAngle(center), 1e-6)
 
         const point2 = Point(4 + Math.sqrt(3), 6)
-        assertEqual(Math.PI / 6, point2.measureAngle(center), 1e-6)
+        UnitTest.assertEqual(Math.PI / 6, point2.measureAngle(center), 1e-6)
 
         const point3 = Point(5, 6)
-        assertEqual(Math.PI / 4, point3.measureAngle(center), 1e-6)
+        UnitTest.assertEqual(Math.PI / 4, point3.measureAngle(center), 1e-6)
 
         const point4 = Point(5, 5 + Math.sqrt(3))
-        assertEqual(Math.PI / 3, point4.measureAngle(center), 1e-6)
+        UnitTest.assertEqual(Math.PI / 3, point4.measureAngle(center), 1e-6)
 
         const point5 = Point(4, 6)
-        assertEqual(Math.PI / 2, point5.measureAngle(center), 1e-6)
+        UnitTest.assertEqual(Math.PI / 2, point5.measureAngle(center), 1e-6)
 
         const point6 = Point(3, 5)
-        assertEqual(Math.PI, point6.measureAngle(center), 1e-6)
+        UnitTest.assertEqual(Math.PI, point6.measureAngle(center), 1e-6)
     }
 
     testMeasureAngleSamePoint() {
         const origin = Point(0, 0)
-        assertEqual(0, origin.measureAngle(), 1e-6)
+        UnitTest.assertEqual(0, origin.measureAngle(), 1e-6)
 
         const point = Point(4, 5)
-        assertEqual(0, point.measureAngle(point), 1e-6)
+        UnitTest.assertEqual(0, point.measureAngle(point), 1e-6)
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,92 @@
 "use strict";
 
+class UnitTest {
+    constructor(tests = []) {
+        this.tests = tests
+
+    }
+
+    static AssertError(expected, value) {
+        const error = {
+            name: 'AssertError',
+            message: `The value ${JSON.stringify(value)}(${typeof value}) is expected to be ${JSON.stringify(expected)}(${typeof expected}) ${(new Error).stack.split(/[\r\n]+/)[3]}.`,
+        }
+        error.prototype = Error.prototype
+        return error
+    }
+
+    run() {
+        const results = document.getElementById('results')
+
+        this.tests.forEach(function(testClass) {
+            const testObject = new testClass()
+            results.innerHTML += `<tr><th colspan="2">${testClass.name||'Please provide a class name'}</th></tr>`
+            Object.getOwnPropertyNames(testClass.prototype).forEach(function(property) {
+                if (property != 'constructor' && testObject[property] instanceof Function) {
+                    try {
+                        testObject[property]()
+                        results.innerHTML += `<tr><td>${property}()</td><td style="background:green;color:white">OK</td></tr>`
+                    } catch (e) {
+                        if (e.name == 'AssertError') {
+                            results.innerHTML += `<tr><td>${property}()</td><td style="background:red;color:white">${e.message}</td></tr>`
+                        } else {
+                            results.innerHTML += `<tr><td colspan="2" style="background:red;color:white;font-weight:bold;white-space:pre">${e.message} ${(new Error).stack}</td></tr>`
+                        }
+                    }
+                }
+            })
+        })
+    }
+
+    static assertDefined(value) {
+        if (value === undefined) throw this.AssertError('defined', value)
+    }
+
+    static assertUndefined(value) {
+        UnitTest.assertEqual(undefined, value)
+    }
+
+    static assertTrue(value) {
+        UnitTest.assertEqual(true, value)
+    }
+
+    static assertFalse(value) {
+        UnitTest.assertEqual(false, value)
+    }
+
+    static assertEqual(expected, value, limit = 0) {
+        try {
+            if (typeof expected !== typeof value) throw Error
+            switch (typeof expected) {
+                case 'array':
+                    if (expected.length != value.length) throw Error
+                    for (let i = 0; i < expected.length; i++) {
+                        UnitTest.assertEqual(expected[i], value[i], limit)
+                    }
+                    break
+
+                case 'object':
+                    Object.getOwnPropertyNames(expected).forEach(function(property) {
+                        if (typeof expected[property] === 'function') return
+                        if (!value.hasOwnProperty(property)) throw Error
+                        UnitTest.assertEqual(expected[property], value[property], limit)
+                    })
+                    break
+
+                case 'number':
+                    if (Math.abs(expected - value) > limit) throw Error
+                    break
+
+                default:
+                    if (expected !== value) throw Error
+            }
+        } catch (e) {
+            console.log(e.stack);
+            throw this.AssertError(expected, value)
+        }
+    }
+}
+/*
 const runUnitTests = function(tests = []) {
     const results = document.getElementById('results')
 
@@ -78,3 +165,4 @@ const AssertError = function(expected, value) {
     error.prototype = Error.prototype
     return error
 }
+*/
