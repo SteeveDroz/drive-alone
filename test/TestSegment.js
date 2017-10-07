9eb62dbc89f0d20a85cee29a9aaa6345a79d765d
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-10-07 19:56:52 +0200

    Start segment test

--- test/TestSegment.js
+++ test/TestSegment.js
@@ -1,3 +1,8 @@
 class TestSegment {
-
+    testConstructor() {
+        const segment = Segment(Point(0, 0), Point(1, 1))
+        UnitTest.assertDefined(segment)
+        UnitTest.assertEqual(Point(0, 0), segment.start)
+        UnitTest.assertEqual(Point(1, 1), segment.end)
+    }
 }
