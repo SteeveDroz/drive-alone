6f741a9b621b5b7e8526a977410ee2961564f7c8
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-10-08 06:43:43 +0200

    Improve line identification

--- test/UnitTest.js
+++ test/UnitTest.js
@@ -6,9 +6,11 @@ class UnitTest {
     }
 
     static AssertError(expected, value) {
+        const stack = (new Error).stack.split(/[\r\n]+/)
+        const message = stack.filter(line => !line.match(/\/UnitTest\.js:\d+:\d+\)$/))[1].trim()
         const error = {
             name: 'AssertError',
-            message: `The value ${JSON.stringify(value)}(${typeof value}) is expected to be ${JSON.stringify(expected)}(${typeof expected}) ${(new Error).stack.split(/[\r\n]+/)[3]}.`,
+            message: `The value ${JSON.stringify(value)}(${typeof value}) is expected to be ${JSON.stringify(expected)}(${typeof expected}) ${message}`,
         }
         error.prototype = Error.prototype
         return error
@@ -84,88 +86,8 @@ class UnitTest {
                     if (expected !== value) throw Error
             }
         } catch (e) {
-            console.log(e.stack)
+            console.log((new Error).stack)
             throw this.AssertError(expected, value)
         }
     }
 }
-/*
-const runUnitTests = function(tests = []) {
-    const results = document.getElementById('results')
-
-    tests.forEach(function(testClass) {
-        const testObject = new testClass()
-        results.innerHTML += `<tr><th colspan="2">${testClass.name||'Please provide a class name'}</th></tr>`
-        Object.getOwnPropertyNames(testClass.prototype).forEach(function(property) {
-            if (property != 'constructor' && testObject[property] instanceof Function) {
-                try {
-                    testObject[property]()
-                    results.innerHTML += `<tr><td>${property}()</td><td style="background:green;color:white">OK</td></tr>`
-                } catch (e) {
-                    if (e.name == 'AssertError') {
-                        results.innerHTML += `<tr><td>${property}()</td><td style="background:red;color:white">${e.message}</td></tr>`
-                    } else {
-                        results.innerHTML += `<tr><td colspan="2" style="background:red;color:white;font-weight:bold;white-space:pre">${e.message} ${(new Error).stack}</td></tr>`
-                    }
-                }
-            }
-        })
-    })
-}
-
-const assertDefined = function(value) {
-    if (value === undefined) throw AssertError('defined', value)
-}
-
-const assertUndefined = function(value) {
-    if (value !== undefined) throw AssertError(undefined, value)
-}
-
-const assertTrue = function(value) {
-    if (!value) throw AssertError(true, value)
-}
-
-const assertFalse = function(value) {
-    if (value) throw AssertError(true, value)
-}
-
-const assertEqual = function(expected, value, limit = 0) {
-    try {
-        if (typeof expected !== typeof value) throw Error
-        switch (typeof expected) {
-            case 'array':
-                if (expected.length != value.length) throw Error
-                for (let i = 0; i < expected.length; i++) {
-                    assertEqual(expected[i], value[i], limit)
-                }
-                break
-
-            case 'object':
-                Object.getOwnPropertyNames(expected).forEach(function(property) {
-                    if (typeof expected[property] === 'function') return
-                    if (!value.hasOwnProperty(property)) throw Error
-                    assertEqual(expected[property], value[property], limit)
-                })
-                break
-
-            case 'number':
-                if (Math.abs(expected - value) > limit) throw Error
-                break
-
-            default:
-                if (expected !== value) throw Error
-        }
-    } catch (e) {
-        throw AssertError(expected, value)
-    }
-}
-
-const AssertError = function(expected, value) {
-    const error = {
-        name: 'AssertError',
-        message: `The value ${JSON.stringify(value)}(${typeof value}) is expected to be ${JSON.stringify(expected)}(${typeof expected}) ${(new Error).stack.split(/[\r\n]+/)[3]}.`,
-    }
-    error.prototype = Error.prototype
-    return error
-}
-*/
