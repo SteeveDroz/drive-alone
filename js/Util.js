9b1d082bc65266c92654fa7eb5f000fc5f99f9c0
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-10-18 19:02:06 +0200

    Test Util.map

--- js/Util.js
+++ js/Util.js
@@ -2,6 +2,7 @@
 
 class Util {
     static map(value, src, dest) {
-        return 0
+        const percentage = (value - src[0]) / (src[1] - src[0])
+        return dest[0] + percentage * (dest[1] - dest[0])
     }
 }
--- test/TestUtil.js
+++ test/TestUtil.js
@@ -2,6 +2,15 @@
 
 class TestUtil {
     testMap() {
-
+        UnitTest.assertEqual(13, Util.map(0.3, [0, 1], [10, 20]), 1e-6)
+        UnitTest.assertEqual(10, Util.map(0, [0, 1], [10, 20]), 1e-6)
+        UnitTest.assertEqual(20, Util.map(1, [0, 1], [10, 20]), 1e-6)
+        UnitTest.assertEqual(9, Util.map(-0.1, [0, 1], [10, 20]), 1e-6)
+        UnitTest.assertEqual(0.3, Util.map(13, [10, 20], [0, 1]), 1e-6)
+        UnitTest.assertEqual(0, Util.map(10, [10, 20], [0, 1]), 1e-6)
+        UnitTest.assertEqual(1, Util.map(20, [10, 20], [0, 1]), 1e-6)
+        UnitTest.assertEqual(-0.1, Util.map(9, [10, 20], [0, 1]), 1e-6)
+        UnitTest.assertEqual(0.7, Util.map(3, [10, 0], [0, 1]), 1e-6)
+        UnitTest.assertEqual(0.7, Util.map(3, [0, 10], [1, 0]), 1e-6)
     }
 }
